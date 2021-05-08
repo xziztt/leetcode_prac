a099d32d2e0dd6a85dea6ba8a09f0edf87622a10
xziztt <vigneshmvm2000@gmail.com>
2021-05-08 11:54:17 +0530

    added toeplitz_matrix

---
+++ toeplitz_matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+
+        for(int i = 0;i < matrix.size();i++){
+            for(int j = 0;j < matrix[0].size();j++){
+                if(i > 0 && j > 0){
+                    if(matrix[i][j] != matrix[i-1][j-1]){
+                        return false;
+
+
+                }
+
+                }
+            }
+        }
+        return true;
+    }
+};

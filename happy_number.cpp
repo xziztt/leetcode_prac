86425f903330970921e56bc6ad0896776d71a258
xziztt <vigneshmvm2000@gmail.com>
2021-05-10 16:11:09 +0530

    added happy_number

---
+++ happy_number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    int returnSum(int num){
+        int sum = 0;
+        while(num > 0){
+            int temp =  num%10;
+            num/=10;
+            sum += temp*temp;
+        }
+        return sum;
+    }
+    bool isHappy(int n) {
+        int slow = n,fast = n;
+        do{
+            slow = returnSum(slow);
+            fast = returnSum(fast);
+            fast = returnSum(fast);
+            if(fast == 1)return true;
+        }while(fast!=slow);
+        return false;
+    }
+};
